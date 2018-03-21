818ec5787a803a55686698f6a6d13b3220a887c2
Joseph Chingalo <profschingalo@gmail.com>
2018-03-21 16:15:03 +0300

    Render list of entry form to select as well as retrive list of data sets selected on check or uncheck data sets

--- src/pages/sms-gateway/components/sms-gateway-dataset-setup/sms-gateway-dataset-setup.ts
+++ src/pages/sms-gateway/components/sms-gateway-dataset-setup/sms-gateway-dataset-setup.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SmsGatewayProvider } from '../../../../providers/sms-gateway/sms-gateway';
 import { DataSetsProvider } from '../../../../providers/data-sets/data-sets';
 import { CurrentUser } from '../../../../models/currentUser';
+import { SmsConfiguration } from '../../../../models/smsCommand';
+import { DataSet } from '../../../../models/dataSet';
+import * as _ from 'lodash';
 
 /**
  * Generated class for the SmsGatewayDatasetSetupComponent component.
@@ -14,15 +17,49 @@ import { CurrentUser } from '../../../../models/currentUser';
   templateUrl: 'sms-gateway-dataset-setup.html'
 })
 export class SmsGatewayDatasetSetupComponent implements OnInit {
-  dataSets: Array<any>;
   @Input() currentUser: CurrentUser;
+  dataSets: Array<any>;
+  isLoading: boolean;
 
   constructor(
     private smsGatewayProvider: SmsGatewayProvider,
     private dataSetProvider: DataSetsProvider
   ) {
+    this.isLoading = true;
     this.dataSets = [];
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.smsGatewayProvider.getSmsConfigurations(this.currentUser).subscribe(
+      (smConfiguration: SmsConfiguration) => {
+        this.dataSetProvider.getAllDataSets(this.currentUser).subscribe(
+          (dataSets: Array<DataSet>) => {
+            dataSets.map((dataSet: DataSet) => {
+              this.dataSets.push({
+                id: dataSet.id,
+                name: dataSet.name,
+                status:
+                  smConfiguration.dataSetIds.indexOf(dataSet.id) > -1
+                    ? true
+                    : false
+              });
+            });
+            this.isLoading = false;
+          },
+          error => {
+            this.isLoading = false;
+          }
+        );
+      },
+      error => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  updateSelectedItems() {
+    const seletectDataSets = _.filter(this.dataSets, { status: true });
+    const dataSetIds = _.map(seletectDataSets, 'id');
+    console.log(dataSetIds);
+  }
 }
